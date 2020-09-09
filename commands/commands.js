35ec87fff561124391d06c5a0066650579c9bf51
sweqtyy <58605906+sweqtyy@users.noreply.github.com>
2020-09-09 03:58:53 -0400

    Command Folder
    A advanced way of calling commands :-)

---
+++ commands/commands.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "commands",
+    description: "Shows every avaliable command.",
+    execute(message, args){
+        const Discord = require('discord.js');
+        const bot = new Discord.Client();
+
+        const embed = new Discord.MessageEmbed()
+
+    .setTitle('Command Help')
+    .setColor('#fff2f2')
+    .setDescription("Current commands available for this bot:\n**!!ping** - Pings the bot, checking its response time.\n**!!help** - Shows this menu.\n\nMore commands to use coming soon!")
+     .setFooter(text = "made by sweqtyy#0001.", iconURL = 'https://media.giphy.com/media/JogsJZQiuEg5NvfRNL/giphy.gif')
+        
+
+    message.channel.send(embed)  
+ }
+}
