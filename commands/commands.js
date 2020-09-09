390ee655a9b911dc265ea6de245ac6211e0d8b3e
sweqtyy <58605906+sweqtyy@users.noreply.github.com>
2020-09-09 15:41:46 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -9,7 +9,7 @@ module.exports = {
 
     .setTitle('Command Help')
     .setColor('#fff2f2')
-    .setDescription("Current commands available for this bot:\n**!!ping** - Pings the bot, checking its response time.\n**!!help** - Shows this menu.\n**!!avatar** - Shows users avatar. (To see another users avatar, simply mention them.)\n\nModeration Commands:\n**!!slowmode [TIME IN SECONDS]** - Still a W.I.P, but slowmode is calculated in seconds.\n\nMore commands to use coming soon!")
+    .setDescription("Current commands available for this bot:\n**!!ping** - Pings the bot, checking its response time.\n**!!help** - Shows this menu.\n**!!avatar** - Shows users avatar. (To see another users avatar, simply mention them.)\n\nModeration Commands:\n**!!slowmode [TIME IN SECONDS]** - Adds slowmode to current channel. (In seconds.) \n\nMore commands to use coming soon!")
      .setFooter(text = "made by sweqtyy#0001.", iconURL = 'https://media.giphy.com/media/JogsJZQiuEg5NvfRNL/giphy.gif')
         
 
